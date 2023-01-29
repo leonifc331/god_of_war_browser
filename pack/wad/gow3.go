0e1cab8179a6fa8fd8d3fdbe71a12172ffad530d
Re-Play Games <37820098+leonifc331@users.noreply.github.com>
2023-01-29 15:30:12 -0300

    Update gow3.go

--- pack/wad/gow3.go
+++ pack/wad/gow3.go
@@ -3,25 +3,25 @@ package wad
 import "fmt"
 
 const (
-	TAG_GOW2_ENTITY_COUNT     = 0
-	TAG_GOW2_SERVER_INSTANCE  = 1
-	TAG_GOW2_FILE_GROUP_START = 2
-	TAG_GOW2_FILE_GROUP_END   = 3
+	TAG_GOW3_ENTITY_COUNT     = 0
+	TAG_GOW3_SERVER_INSTANCE  = 1
+	TAG_GOW3_FILE_GROUP_START = 2
+	TAG_GOW3_FILE_GROUP_END   = 3
 
-	TAG_GOW2_HEADER_START = 21
-	TAG_GOW2_HEADER_POP   = 19
+	TAG_GOW3_HEADER_START = 21
+	TAG_GOW3_HEADER_POP   = 19
 
-	TAG_GOW2_TT_11 = 11
-	TAG_GOW2_TT_12 = 12
-	TAG_GOW2_TT_13 = 13
-	TAG_GOW2_TT_14 = 14
-	TAG_GOW2_TT_15 = 15
-	TAG_GOW2_TT_16 = 16
+	TAG_GOW3_TT_11 = 11
+	TAG_GOW3_TT_12 = 12
+	TAG_GOW3_TT_13 = 13
+	TAG_GOW3_TT_14 = 14
+	TAG_GOW3_TT_15 = 15
+	TAG_GOW3_TT_16 = 16
 )
 
 func (w *Wad) gow2parseTag(tag *Tag, currentNode *NodeId, newGroupTag *bool, addNode func(tag *Tag) *Node) error {
 	switch tag.Tag {
-	case TAG_GOW2_SERVER_INSTANCE: // file data packet
+	case TAG_GOW3_SERVER_INSTANCE: // file data packet
 		// Tell file server (server determined by first uint16)
 		// that new file is loaded
 		// if name start with space, then name ignored (unnamed)
@@ -31,9 +31,9 @@ func (w *Wad) gow2parseTag(tag *Tag, currentNode *NodeId, newGroupTag *bool, add
 			*newGroupTag = false
 			*currentNode = n.Id
 		}
-	case TAG_GOW2_FILE_GROUP_START:
+	case TAG_GOW3_FILE_GROUP_START:
 		*newGroupTag = true
-	case TAG_GOW2_FILE_GROUP_END:
+	case TAG_GOW3_FILE_GROUP_END:
 		if !*newGroupTag {
 			if *currentNode == NODE_INVALID {
 				return fmt.Errorf("Trying to end not started group id%d-%s", tag.Id, tag.Name)
@@ -42,7 +42,7 @@ func (w *Wad) gow2parseTag(tag *Tag, currentNode *NodeId, newGroupTag *bool, add
 		} else {
 			*newGroupTag = false
 		}
-	case TAG_GOW2_TT_11, TAG_GOW2_TT_12, TAG_GOW2_TT_13, TAG_GOW2_TT_14, TAG_GOW2_TT_15, TAG_GOW2_TT_16:
+	case TAG_GOW3_TT_11, TAG_GOW3_TT_12, TAG_GOW3_TT_13, TAG_GOW3_TT_14, TAG_GOW3_TT_15, TAG_GOW3_TT_16:
 		/*
 			tag11:
 				unk 4 bytes (32 CB 08 4A)
@@ -58,7 +58,7 @@ func (w *Wad) gow2parseTag(tag *Tag, currentNode *NodeId, newGroupTag *bool, add
 				description of some fields in tweak template?
 		*/
 		addNode(tag)
-	case TAG_GOW2_HEADER_START:
+	case TAG_GOW3_HEADER_START:
 		addNode(tag)
 	default:
 		// return fmt.Errorf("unknown tag id%.4x-tag%.4x-%s offset 0x%.6x", tag.Id, tag.Tag, tag.Name, tag.DebugPos)
