0d5cbc7e901f8560b39f081f353b06c598f16ae0
Re-Play Games <37820098+leonifc331@users.noreply.github.com>
2024-01-01 23:48:12 -0300

    Update actions.go

--- pack/wad/mesh/actions.go
+++ pack/wad/mesh/actions.go
@@ -15,29 +15,17 @@ func (mesh *Mesh) HttpAction(wrsrc *wad.WadNodeRsrc, w http.ResponseWriter, r *h
 		webutils.WriteFileHeaders(w, wrsrc.Tag.Name+".fbx")
 		if err := mesh.ExportFbxDefault(wrsrc).Write(w); err != nil {
 			log.Printf("Error when exporting mesh as fbx: %v", err)
-		}else {
-			if err := fbxutils.ExportBinary(w, doc); err != nil {
-				log.Printf("Failed to encode fbx: %v", err)
-			}
 		}
 	case "obj":
 		webutils.WriteFileHeaders(w, wrsrc.Tag.Name+".obj")
 		if err := mesh.ExportObj(w, nil); err != nil {
 			log.Printf("Error when exporting mesh as obj: %v", err)
-		}else {
-			if err := objutils.ExportBinary(w, doc); err != nil {
-				log.Printf("Failed to encode obj: %v", err)
-			}
 		}
 	case "gltf":
 		webutils.WriteFileHeaders(w, wrsrc.Tag.Name+".glb")
 
 		if doc, err := mesh.ExportGLTFDefault(wrsrc); err != nil {
 			log.Printf("Error when exporting mesh as gltf: %v", err)
-		} else {
-			if err := gltfutils.ExportBinary(w, doc); err != nil {
-				log.Printf("Failed to encode gltf: %v", err)
-			}
 		}
 	}
 }
