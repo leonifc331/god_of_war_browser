bbb120fe7002983c3745a1d928b9c94a5058eeb7
Re-Play Games <37820098+leonifc331@users.noreply.github.com>
2024-01-27 16:46:52 -0300

    Update actions.go

--- pack/wad/mesh/actions.go
+++ pack/wad/mesh/actions.go
@@ -9,14 +9,27 @@ import (
 	"github.com/mogaika/god_of_war_browser/webutils"
 )
 
-func (mesh *Mesh) HttpAction(wrsrc *wad.WadNodeRsrc, w http.ResponseWriter, r *http.Request) {
-	webutils.WriteFileHeaders(w, wrsrc.Tag.Name+".obj")
-	if err := mesh.ExportObj(w, nil); err != nil {
-		log.Printf("Error when exporting mesh as obj: %v", err)
-	}
+func (mesh *Mesh) HttpAction(wrsrc *wad.WadNodeRsrc, w http.ResponseWriter, r *http.Request, action string) {
+	switch action {
+	case "fbx":
+		webutils.WriteFileHeaders(w, wrsrc.Tag.Name+".fbx")
+		if err := mesh.ExportFbxDefault(wrsrc).Write(w); err != nil {
+			log.Printf("Error when exporting mesh as fbx: %v", err)
+		}
+	case "obj":
+		webutils.WriteFileHeaders(w, wrsrc.Tag.Name+".obj")
+		if err := mesh.ExportObj(w, nil); err != nil {
+			log.Printf("Error when exporting mesh as obj: %v", err)
+		}
+	case "gltf":
+		webutils.WriteFileHeaders(w, wrsrc.Tag.Name+".glb")
 
-	webutils.WriteFileHeaders(w, wrsrc.Tag.Name+".glb")
-	if _, err := mesh.ExportGLTFDefault(wrsrc); err != nil {
-		log.Printf("Error when exporting mesh as gltf: %v", err)
+		if doc, err := mesh.ExportGLTFDefault(wrsrc); err != nil {
+			log.Printf("Error when exporting mesh as gltf: %v", err)
+		} else {
+			if err := gltfutils.ExportBinary(w, doc); err != nil {
+				log.Printf("Failed to encode gltf: %v", err)
+			}
+		}
 	}
 }
